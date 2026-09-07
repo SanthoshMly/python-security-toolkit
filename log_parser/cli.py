6e5e40f79606f9e5a408c448717498626f56f9d4
santhosh <santhoshkulal849@gmail.com>
2026-09-07 13:36:27 +0530

    Added a logfile argument that tells the argparse that a user must provide a logfile for analysis.

--- log_parser/cli.py
+++ log_parser/cli.py
@@ -8,6 +8,9 @@ def main():
     # Create the parser
     parser = argparse.ArgumentParser(description="Apache/Nginx Security Log Analyzer")
 
+    # Add the logfile argument, tells argpasre that user must provide a log file to analyze
+    parser.add_argument("logfile", help="Path to Apache/Nginx access log")
+
 
 if __name__ == "__main__":
     main()
