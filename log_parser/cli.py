dd899ec823b3f9cd618105e4a3bf3f72ad2b4bc8
santhosh <santhoshkulal849@gmail.com>
2026-09-07 13:43:03 +0530

    A command-line flag that becomes true when included and becomes false when excluded.

--- log_parser/cli.py
+++ log_parser/cli.py
@@ -11,6 +11,11 @@ def main():
     # Add the logfile argument, tells argpasre that user must provide a log file to analyze
     parser.add_argument("logfile", help="Path to Apache/Nginx access log")
 
+    # optional argument to show only suspicious requests
+    parser.add_argument(
+        "--suspicious", action="store_true", help="Show only suspicious requests"
+    )
+
 
 if __name__ == "__main__":
     main()
