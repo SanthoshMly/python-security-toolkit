c09d354c8c8c59bdf48a55d5822e63943edadb61
santhosh <santhoshkulal849@gmail.com>
2026-09-07 13:33:38 +0530

    Creating the argument parser

---
+++ log_parser/cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+from log_parser.parser import parse_file, generate_summary
+
+
+def main():
+
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Apache/Nginx Security Log Analyzer")
+
+
+if __name__ == "__main__":
+    main()
